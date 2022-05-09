f338897ee4243f320058e2a36971a048841939c3
mozaicdb <57225767+mozaicdb@users.noreply.github.com>
2022-05-08 23:36:42 -0700

    dash

---
+++ orders.js
@@ -0,0 +1,38 @@
+const orders = [{
+        productName: 'redmik20',
+        productNumber: '85631',
+        paymentstatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'applephone1',
+        productNumber: '36375',
+        paymentstatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Nokia Phone',
+        productNumber: '36378',
+        paymentstatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Samsung Phone',
+        productNumber: '49347',
+        paymentstatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'infinix Phone',
+        productNumber: '96996',
+        paymentstatus: 'Due',
+        shipping: 'Pending',
+    },
+
+    {
+        productName: 'SamSungGalaxyNote20',
+        productNumber: '22821',
+        paymentstatus: 'Due',
+        shipping: 'Delivered',
+    },
+];
